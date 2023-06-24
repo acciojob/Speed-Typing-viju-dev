35f1de813e318bca6f6b8f334e5495a24c499778
viju-dev <71461702+viju-dev@users.noreply.github.com>
2023-06-24 19:56:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let QuotesList={};
+fetch('https://raw.githubusercontent.com/saksham-accio/f2_contest_3/main/food.json')
+  .then(response => response.json())
+  .then(data => {
+    QuotesList = data;
+    data.forEach(item => {
+      
+    })
+});
